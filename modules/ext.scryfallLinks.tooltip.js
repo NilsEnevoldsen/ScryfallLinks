95ea2dabbf15f229b2ccec911076247b00cc5d16
Nils Enevoldsen <NilsEnevoldsen@users.noreply.github.com>
2017-11-14 01:47:30 -0500

    Remove anchor. Add hoverIntent. Scope style.
    Fixes #6.

--- modules/ext.scryfallLinks.tooltip.js
+++ modules/ext.scryfallLinks.tooltip.js
@@ -1,15 +1,15 @@
-$( "a.mw-scryfall-link" ).hover(
+$( "a.mw-scryfall-link" ).hoverIntent(
 	function() {
 		var title = $( this ).text();
 		var cardname = $( this ).attr( "cardname" );
 		if ( $( this ).attr( "cardset" ) ) {
 			var cardset = '&set=' + $( this ).attr( "cardset" );
 		} else {
-			var cardset = "";		
+			var cardset = "";
 		}
 		var popup = new OO.ui.PopupWidget( {
 			$content: $( '<img class="cardimage" alt="' + title + '" width="244" src="https://api.scryfall.com/cards/named?exact=' + cardname + cardset + '&format=image&version=normal">' ),
-			width: 244
+			anchor: false
 		} );
 		$( this ).append( popup.$element );
 		popup.toggle( true );
