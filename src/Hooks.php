6f51b26946163e2bc107002b4dc259865f46bb74
Nils Enevoldsen <NilsEnevoldsen@users.noreply.github.com>
2017-11-16 05:20:37 -0500

    First crack at decks
    #1

--- src/Hooks.php
+++ src/Hooks.php
@@ -17,9 +17,47 @@ class Hooks {
 	 * @return bool
 	 */
 	public static function onParserFirstCallInit( &$parser ) {
+		$parser->setHook( 'deck', 'MediaWiki\Extension\ScryfallLinks\Hooks::renderScryfallDeck' );
 		$parser->setHook( 'c', 'MediaWiki\Extension\ScryfallLinks\Hooks::renderScryfallLink' );
 		$parser->setHook( 'card', 'MediaWiki\Extension\ScryfallLinks\Hooks::renderScryfallLink' );
 		return true;
+	}
+
+	/**
+	 * Render <deck>
+	 * @param string $input Some input
+	 * @param array $args Some args
+	 * @param Parser $parser A parser
+	 * @param PPFrame $frame A PPFrame
+	 * @return string
+	 */
+	public static function renderScryfallDeck( $input, array $args, $parser, $frame ) {
+		// $parser->getOutput()->addModules( 'ext.scryfallLinks.tooltip' );
+		$input = $parser->recursiveTagParse( $input, $frame );
+
+		if ( !$input ) {
+			return '';
+		}
+
+		$decktitle = $args['title'] ?? '';
+		$decktitlespan = '<h3 class="mw-scryfall-decktitle">' . htmlspecialchars( $decktitle ) . '</h3>';
+
+		$decklist = explode( PHP_EOL, $input );
+		$decklist = array_filter( $decklist );
+		$decklist = preg_replace( '/^[^\d].+$/', '<h4 class="mw-scryfall-decksection">$0</h4>',
+			$decklist );
+		$decklist = preg_replace_callback( '/^(\d+)\s+(.+)$/',
+			function ( $m ) {
+				return '<p>' . $m[1] . ' <a href="https://scryfall.com/search?q=%21%22' .
+				htmlspecialchars( urlencode( $m[2] ) ) . '%22" class="mw-scryfall-link" data-card-name="' .
+				htmlspecialchars( urlencode( $m[2] ) ) . '">' . $m[2] . '</a></p>';
+			},
+			$decklist );
+		$decklist = implode( PHP_EOL, $decklist );
+
+		$output = '<div class="mw-scryfall-deck">' . $decktitlespan . $decklist .'</div>';
+
+		return $output;
 	}
 
 	/**
