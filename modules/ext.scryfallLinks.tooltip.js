47ca971fcd1218372ef09d5f9c36ed311081d716
Nils Enevoldsen <NilsEnevoldsen@users.noreply.github.com>
2018-04-25 02:05:31 -0400

    Only load successful images
    Tooltips aren't shown until images are loaded. Card backs are not shown. followCursor is sticky, so I'm disabling it for now. This represents further work on #25.

--- modules/ext.scryfallLinks.tooltip.js
+++ modules/ext.scryfallLinks.tooltip.js
@@ -1,6 +1,6 @@
 ( function () {
 	var el = document.createElement( 'div' );
-	el.style.cssTest = 'display: none;';
+	el.style.display = 'none';
 	el.id = 'js--card-popup';
 	document.body.append( el );
 }() );
@@ -10,7 +10,7 @@ $( function () {
 	const tip = tippy( '.ext-scryfall-link', {
 		arrow: false,
 		animateFill: false,
-		followCursor: true,
+		// followCursor: true,
 		html: '#js--card-popup',
 		placement: 'bottom',
 		interactive: true,
@@ -22,17 +22,26 @@ $( function () {
 		theme: 'scryfall',
 		onShow() {
 			// `this` inside callbacks refers to the popper element
+			var self = this,
+				cardImage = new Image( 244 );
+			/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
 			const target = this._reference,
-				title = target.text,
+				anchorElement = '<a href="' + target.href + '"><img class="ext-scryfall-placeholder"></a>',
 				cardNameQuery = '&exact=' + target.dataset.cardName,
 				cardSet = target.dataset.cardSet,
 				cardSetQuery = cardSet ? '&set=' + cardSet : '',
 				formatQuery = '&format=image',
 				versionQuery = '&version=normal',
-				imageSrc = 'https://api.scryfall.com/cards/named?' + cardNameQuery + cardSetQuery + formatQuery + versionQuery,
-				imageElement = '<img class="ext-scryfall-cardimage" width="244" alt="' + title + '" src="' + imageSrc + '">',
-				anchorElement = '<a href="' + target.href + '">' + imageElement + '</a>';
+				imageSrc = 'https://api.scryfall.com/cards/named?' + cardNameQuery + cardSetQuery + formatQuery + versionQuery;
+			this.style.display = 'none';
 			this.querySelector( '.tippy-content' ).innerHTML = anchorElement;
+			cardImage.alt = target.text;
+			cardImage.className = 'ext-scryfall-cardimage';
+			cardImage.onload = function () {
+				$( '.ext-scryfall-placeholder' ).replaceWith( cardImage );
+				self.style.display = '';
+			};
+			cardImage.src = imageSrc;
 		},
 		onHidden() {
 			this.querySelector( '.tippy-content' ).innerHTML = '';
